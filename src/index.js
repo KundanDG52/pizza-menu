c35b35253b465cb4aa3f7fbe6df27a254d05fb24
Kundan Gahalot <gahalotkundan@gmail.com>
2025-08-07 11:30:44 +0530

    updates in order title, logo and order timing

--- src/index.js
+++ src/index.js
@@ -118,7 +118,7 @@ function Pizza({ pizzaObj }) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 10;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
